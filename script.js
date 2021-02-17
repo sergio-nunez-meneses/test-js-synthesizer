6f747be99460d9d9e3c9722f411d632ef1ecb1a9
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-17 09:42:24 +0100

    turned osc function into a class; added attack and release methods, and a keydown tracker; update keydown and keyup event listeners

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
     74: 493.8833,
     75: 523.2511
   },
-  voices = {},
-  voice;
+  notesOn = {},
+  note;
 
 function scaleInput(input, r1, r2) {
   return (input - r1[0]) * (r2[1] - r2[0]) / (r1[1] - r1[0]) + r2[0];
@@ -36,20 +36,35 @@ function validKeys(e, key) {
   return key;
 }
 
-function osc(wave, freq, amp) {
-  var osc = audioCtx.createOscillator(),
-    gainNode = audioCtx.createGain();
+class Voice {
+  constructor(wave, freq, amp) {
+    this.wave = wave;
+    this.freq = freq;
+    this.amp = amp;
+    this.voices = [];
+  }
 
-  osc.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
+  attack = function() {
+    var osc = audioCtx.createOscillator();
+    osc.type = this.wave;
+    osc.frequency.value = this.freq;
 
-  osc.type = wave;
-  osc.frequency.value = freq;
-  gainNode.gain.value = amp;
-  osc.start();
+    var gainNode = audioCtx.createGain();
+    gainNode.gain.value = this.amp;
 
-  return [osc, gainNode];
-}
+    osc.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    osc.start(0);
+    this.voices.push(osc);
+  }
+
+  release = function() {
+    this.voices.forEach((osc) => {
+      osc.stop();
+    });
+  }
+};
 
 document.addEventListener('keydown', (e) => {
   var key = validKeys(e, e.keyCode);
@@ -58,17 +73,14 @@ document.addEventListener('keydown', (e) => {
     return;
   }
 
-  if (voices[key]) {
-    return;
-  }
+  var dB = scaleInput(-30, [-75, 0], [0, 1]);
 
-  var dB = scaleInput(-18, [-75, 0], [0, 1]);
+  note = new Voice('square', freqs[key], dB);
+  notesOn[key] = note;
+  note.attack();
 
-  voice = osc('square', freqs[key], dB);
-  voices[key] = e.type == 'keydown';
-
-  console.log('pressed:', voice);
-  console.log('pressed:', voices);
+  console.log('pressed:', note);
+  console.log('pressed:', notesOn);
 });
 
 document.addEventListener('keyup', (e) => {
@@ -78,12 +90,8 @@ document.addEventListener('keyup', (e) => {
     return;
   }
 
-  if (voices[key]) {
-    voice[1].disconnect();
-    voice[0].stop(voice[0].context.currentTime);
-    voices[key] = e.type == 'keydown';
-  }
+  notesOn[key].release();
+  delete notesOn[key];
 
-  console.log('released:', voice);
-  console.log('released:', voices);
+  console.log('released:', notesOn);
 });
